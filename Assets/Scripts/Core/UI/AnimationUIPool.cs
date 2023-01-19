a6a029c960987bce36bb0ccf9b92615b84f0a9f2
DESKTOP-J0N8RSI\Deniz <d.serhatisikli@gmail.com>
2023-01-19 18:43:09 +0300

    code refactor

---
+++ Assets/Scripts/Core/UI/AnimationUIPool.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Utility.Pool;
+
+namespace Core.UI
+{
+    public class AnimationUIPool : MonoBehaviour, IResettable
+    {
+        public void Reset()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
